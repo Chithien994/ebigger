159b72f3b9e73fc6636c4e5b7c95573e2273ef4b
DESKTOP-VF0P4M8\Thien <chithien994@gmail.com>
2018-09-19 16:39:20 +0700

    up

--- apps/models.py
+++ apps/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class AppInfo(models.Model):
-	key 	= models.CharField(max_length=255, unique=True)
+	key 	= models.CharField(max_length=100, unique=True)
 	value 	= models.TextField(default='', blank=True, null=True)
 	def __str__(self):
-		return '%s-%s' % (self.pk, self.key)
+		return '%s#%s' % (self.pk, self.key)
 
 	class Meta:
 		db_table 			= 'appinfo'
